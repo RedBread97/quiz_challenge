19c34de36e8345089467dc7e7e138ef1b68cfbcc
Alex Lane <97alane@gmail.com>
2022-03-29 18:16:17 -0500

    set up game over screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-
-
+var formEl = document.getElementById('form');
+var newScore = {
+    "score": 0,
+    "initials":""
+}
+var scores = JSON.parse(localStorage.getItem('scores'));
 var questionIndex = 0;
 const myQuestions = [
     {
@@ -25,16 +29,16 @@ const myQuestions = [
         answers: {
             a: "Angular",
             b: "jQuery",
-            c: "RequireJS",
-            d: "ESLint"
+            c: "ESLint"
+
         },
-        correctAnswer: "d"
+        correctAnswer: "c"
     }
 ];
 
 var timerElement = document.querySelector(".timer-count");
 
-var submitBtn = '<button type="button" class="btn btn-primary" id="submit">Submit</button>'
+var submitBtn = '<button type="button" class="btn btn-primary" id="submit">Submit</button>';
 var timerCount = 60;
 
 var form = ' <h2>' + myQuestions[questionIndex].question +
@@ -46,8 +50,9 @@ var form = ' <h2>' + myQuestions[questionIndex].question +
 document.getElementById('start').addEventListener("click", function () {
 
     document.getElementById('start').style.display = 'none';
+    
     document.getElementById('introText').style.display = 'none';
-    document.getElementById("form").insertAdjacentHTML("afterbegin", form);
+    formEl.insertAdjacentHTML("afterbegin", form);
     document.getElementById('btn').insertAdjacentHTML("afterbegin", submitBtn);
 
     startTimer();
@@ -56,31 +61,65 @@ document.getElementById('start').addEventListener("click", function () {
 
 function submit() {
     document.getElementById('submit').addEventListener("click", function () {
-       
+
         var a = document.querySelector('input[name=flexRadioDefault]:checked').getAttribute('id');
         if (a == myQuestions[questionIndex].correctAnswer) {
             console.log("Yay right answer!");
+            newScore.score++;
             questionIndex++;
-            form = ' <h2>' + myQuestions[questionIndex].question +
-            '</h2><form><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="a"><label class="form-check-label" for="flexRadioDefault1">' + myQuestions[questionIndex].answers.a +
-            '</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="b"checked><label class="form-check-label" for="flexRadioDefault2">' + myQuestions[questionIndex].answers.b +
-            '</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="c"checked><label class="form-check-label" for="flexRadioDefault2">' + myQuestions[questionIndex].answers.c +
-            '</label></div></form>';
-        
-            document.getElementById('form').innerHTML = '';
-            document.getElementById("form").insertAdjacentHTML("afterbegin", form);
+            formEl.innerHTML = '';
+            if (questionIndex < myQuestions.length) {
+                form = ' <h2>' + myQuestions[questionIndex].question +
+                    '</h2><form><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="a"><label class="form-check-label" for="flexRadioDefault1">' + myQuestions[questionIndex].answers.a +
+                    '</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="b"checked><label class="form-check-label" for="flexRadioDefault2">' + myQuestions[questionIndex].answers.b +
+                    '</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="c"checked><label class="form-check-label" for="flexRadioDefault2">' + myQuestions[questionIndex].answers.c +
+                    '</label></div></form>';
+
+
+                formEl.insertAdjacentHTML("afterbegin", form);
+
+            } else {
+                clearInterval(timer);
+                document.getElementById('submit').style.display = 'none';
+                document.getElementById('timer').style.display = 'none';
+                var gameOver = '<h3>GAME OVER!!!</h3><div class="row justify-content-between"><div class="col-3"><label for="inputInitial" class="form-label">initials</label><input type="text" id="inputInitial" class="form-control"></div><div class="col-3"><p> Score-' + newScore.score +
+                    '</p></div></div>'
+                formEl.insertAdjacentHTML("afterbegin", gameOver);
+                var gameBtn = '<button type="button" class="btn btn-primary" id="gameBtn">Submit</button>';
+                document.getElementById('btn').insertAdjacentHTML("afterbegin", gameBtn);
+                document.getElementById('gameBtn').addEventListener("click", function () {
+                    var initials = document.querySelector('input[id=inputInitial]').value;
+                    newScore.initials = initials
+                    console.log(scores)
+                  if (scores == null){
+                    var allScores = [];
+                    allScores.push(newScore);
+                    localStorage.setItem("scores", JSON.stringify(allScores));
+                  }else{
+                      scores.unshift(newScore);
+                      localStorage.setItem("scores", JSON.stringify(scores));
+
+                  }
+                })
+            }
         }
         console.log(a);
     })
 }
 
-//finish setting up timer
 
+
+//finish setting up timer
+var timer;
 function startTimer() {
     // Sets timer
+
     timer = setInterval(function () {
         timerCount--;
-       console.log(timerCount);
+        console.log(timerCount);
+        if (timerCount == 0) {
+            clearInterval(timer);
+        }
     }, 1000);
 }
 
