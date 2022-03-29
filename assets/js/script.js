3785890a110b1dfab5e3cb7e813522ef9ebada8e
Alex Lane <97alane@gmail.com>
2022-03-28 21:48:31 -0500

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,64 @@
 
 
+var questionIndex = 0;
+const myQuestions = [
+    {
+        question: "Who invented JavaScript?",
+        answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one of these is a JavaScript package manager?",
+        answers: {
+            a: "Node.js",
+            b: "TypeScript",
+            c: "npm"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+            a: "Angular",
+            b: "jQuery",
+            c: "RequireJS",
+            d: "ESLint"
+        },
+        correctAnswer: "d"
+    }
+];
+
 var timerElement = document.querySelector(".timer-count");
-var form = ' <h2>placeholder</h2><form><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"><label class="form-check-label" for="flexRadioDefault1">Default radio</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"checked><label class="form-check-label" for="flexRadioDefault2">Default checked radio</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"checked><label class="form-check-label" for="flexRadioDefault2">Default checked radio</label></div></form>';
+
 var submitBtn = '<button type="button" class="btn btn-primary" id="submit">Submit</button>'
 var timerCount = 60;
 
+var form = ' <h2>' + myQuestions[questionIndex].question +
+    '</h2><form><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="a"><label class="form-check-label" for="flexRadioDefault1">' + myQuestions[questionIndex].answers.a +
+    '</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="b"checked><label class="form-check-label" for="flexRadioDefault2">' + myQuestions[questionIndex].answers.b +
+    '</label></div><div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="c"checked><label class="form-check-label" for="flexRadioDefault2">' + myQuestions[questionIndex].answers.c +
+    '</label></div></form>';
+
 document.getElementById('start').addEventListener("click", function () {
+
     document.getElementById('start').style.display = 'none';
-    document.getElementById("form").insertAdjacentHTML("afterend", form);
+    document.getElementById("form").insertAdjacentHTML("afterbegin", form);
     document.getElementById('btn').insertAdjacentHTML("afterbegin", submitBtn);
+    questionIndex++;
+    startTimer()
+    document.getElementById('submit').addEventListener("click", function () {
+        var a = document.querySelector('input[name=flexRadioDefault]:checked').getAttribute('id');
+        if (a == myQuestions[questionIndex].correctAnswer) {
+            console.log("Yay right answer!")
+        }
+        console.log(a);
+    })
 })
+
 
 
 function startTimer() {
@@ -43,34 +92,5 @@ function startTimer() {
     // const quizContainer = document.getElementById('quiz');
     // const resultsContainer = document.getElementById('results');
     // const submitButton = document.getElementById('submit');
-    // const myQuestions = [
-    //   {
-    //     question: "Who invented JavaScript?",
-    //     answers: {
-    //       a: "Douglas Crockford",
-    //       b: "Sheryl Sandberg",
-    //       c: "Brendan Eich"
-    //     },
-    //     correctAnswer: "c"
-    //   },
-    //   {
-    //     question: "Which one of these is a JavaScript package manager?",
-    //     answers: {
-    //       a: "Node.js",
-    //       b: "TypeScript",
-    //       c: "npm"
-    //     },
-    //     correctAnswer: "c"
-    //   },
-    //   {
-    //     question: "Which tool can you use to ensure code quality?",
-    //     answers: {
-    //       a: "Angular",
-    //       b: "jQuery",
-    //       c: "RequireJS",
-    //       d: "ESLint"
-    //     },
-    //     correctAnswer: "d"
-    //   }
-    // ];
+
 
